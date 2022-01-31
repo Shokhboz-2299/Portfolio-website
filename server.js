ce2f20ba9a6d7e7a106e41bea9f99d2f66fd337c
Shokhboz-2299 <shokhbozkaumutov@gmail.com>
2022-01-31 11:14:51 +0500

    new files

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express()
+const mailing = require('./lib/mail')
+const autoReply = require('./lib/autoreply')
 const ejs = require('ejs')
 const PORT = 5000
 const UserController = require('./controllers/userController')
@@ -9,8 +11,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
-app.get('/users', UserController.GET)
-// app.post('/users', UserController.POST)
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', async(req, res) => {
+  const { name, email, text } = req.body
+
+  const messageSent = await mailing(text, email)
+
+
+  if(messageSent) {
+      const messageSentToClient = await autoReply(email, name)
+
+      if(messageSentToClient) {
+          res.send('OK')
+      }
+  }
+})
+
+// app.get('/users', UserController.GET)
 
 
 app.listen(PORT, console.log(`server is running at ${PORT}`))
+
